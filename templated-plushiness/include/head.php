61b42d8cadd6a1db35a88588182b951787b85c23
adrielfabricio <adrielfabricio_oliveira@outlook.com>
2018-08-29 22:51:43 -0300

    deletada pastas inuteis

--- templated-plushiness/include/head.php
+++
@@ -1,8 +0,0 @@
-<head>
-    <title>TecnoJr</title>
-    <link href="http://fonts.googleapis.com/css?family=Source+Sans+Pro:200,300,400,600,700,900" rel="stylesheet" />
-    <link rel="stylesheet" href="css/materialize.css">
-    <link rel="stylesheet" href="css/default.css"/>
-    <link rel="stylesheet" href="css/custom.css">
-    <link rel="stylesheet" href="css/fonts.css"/>
-</head>
